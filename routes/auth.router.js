769fb67596a9960b5c27762b4ad88fa25bf6b11f
MAE776569 <mohamed.ahmed.ezzat776569@gmail.com>
2021-01-17 16:35:21 +0200

    Handle login users

--- routes/auth.router.js
+++ routes/auth.router.js
@@ -53,4 +53,40 @@ router.post(
   }
 )
 
+router.post(
+  "/login",
+  [
+    body("email").isEmail(),
+    body(
+      "password",
+      "Password must consists of minLength: 8, minLowercase: 1, minUppercase: 1, minNumbers: 1, minSymbols: 1"
+    ).isStrongPassword(),
+  ],
+  (req, res, next) => {
+    const errors = validationResult(req).formatWith(errorFormatter)
+    if (!errors.isEmpty())
+      return res.status(422).json({ errors: errors.mapped() })
+
+    passport.authenticate("local", (err, user) => {
+      if (err) return next(err)
+      if (!user) return res.json({ message: "Wrong email or password" })
+
+      req.login(user, (err) => {
+        if (err) return next(err)
+        const { __v, _id, salt, hash, ...data } = user.toJSON()
+        const token = generateAccessToken({
+          id: _id,
+          email: data.email,
+          isAdmin: data.isAdmin,
+        })
+        res.cookie("actk", token, {
+          signed: true,
+          httpOnly: true,
+        })
+        return res.json({ user: data })
+      })
+    })(req, res, next)
+  }
+)
+
 module.exports = router
