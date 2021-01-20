74d64832bbfddab967938f7c466954f42493a17e
MAE776569 <mohamed.ahmed.ezzat776569@gmail.com>
2021-01-20 15:08:21 +0200

    Initialize react app

--- config/server.config.js
+++ config/server.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   env: process.env.NODE_ENV || "development",
-  port: process.env.PORT || 3000,
+  port: process.env.PORT || 5000,
   db: {
     uri: process.env.MONGODB_URI,
   },
