23e912c07c253852b4253ca1a0b2a21c28780aac
MAE776569 <mohamed.ahmed.ezzat776569@gmail.com>
2021-01-23 16:33:36 +0200

    Created category alert

---
+++ src/src/components/CategoryAlert.js
@@ -0,0 +1,47 @@
+import React from "react"
+import {
+  AlertDialog,
+  AlertDialogBody,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogContent,
+  AlertDialogOverlay,
+  Button,
+} from "@chakra-ui/react"
+
+const CategoryAlert = ({ isOpen, onClose, handleDeleteCategory }) => {
+  const cancelRef = React.useRef()
+
+  const confirmDelete = () => {
+    handleDeleteCategory()
+    onClose()
+  }
+
+  return (
+    <AlertDialog
+      isOpen={isOpen}
+      leastDestructiveRef={cancelRef}
+      onClose={onClose}>
+      <AlertDialogOverlay>
+        <AlertDialogContent>
+          <AlertDialogHeader fontSize="lg" fontWeight="bold">
+            Delete Category
+          </AlertDialogHeader>
+          <AlertDialogBody>
+            Are you sure? You can't undo this action afterwards.
+          </AlertDialogBody>
+          <AlertDialogFooter>
+            <Button ref={cancelRef} onClick={onClose}>
+              Cancel
+            </Button>
+            <Button colorScheme="red" onClick={confirmDelete} ml={3}>
+              Delete
+            </Button>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialogOverlay>
+    </AlertDialog>
+  )
+}
+
+export default CategoryAlert
