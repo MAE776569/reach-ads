80eac0d74b9729dee48e9dad01f43edc662e32db
MAE776569 <mohamed.ahmed.ezzat776569@gmail.com>
2021-01-23 16:47:03 +0200

    Allow authentication with header or cookie

--- middlewares/authentication.js
+++ middlewares/authentication.js
@@ -3,12 +3,18 @@ const jwt = require("jsonwebtoken")
 const cookieParser = require("cookie-parser")
 
 function authenticateUser(req, res, next) {
+  // Gather the jwt access token from the request header
+  const authHeader = req.headers["authorization"]
   const authCookie = req.signedCookies["actk"]
+  const tokenHeader = authHeader && authHeader.split(" ")[1]
   const tokenCookie =
     authCookie && cookieParser.signedCookie(authCookie, config.cookies.secret)
 
-  if (!tokenCookie) return res.status(401).json({ message: "Unauthorized" })
-  jwt.verify(tokenCookie, config.jwt.jwtSecret, (err, user) => {
+  if (!tokenHeader && !tokenCookie)
+    return res.status(401).json({ message: "Unauthorized" })
+
+  const token = tokenHeader ? tokenHeader : tokenCookie
+  jwt.verify(token, config.jwt.jwtSecret, (err, user) => {
     if (err) return res.status(403).json({ message: "Forbidden" })
     req.user = user
     return next()
