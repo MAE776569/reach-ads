9790a8a732b7ceea1d4c25963ac07c6329264c3b
MAE776569 <mohamed.ahmed.ezzat776569@gmail.com>
2021-01-23 15:29:17 +0200

    Add default env variables

--- config/server.config.js
+++ config/server.config.js
@@ -5,13 +5,17 @@ module.exports = {
     uri: process.env.MONGODB_URI,
   },
   jwt: {
-    jwtSecret: process.env.JWT_SECRET,
-    maxAge: process.env.JWT_MAX_AGE || "2d",
+    jwtSecret:
+      process.env.JWT_SECRET ||
+      "b33e90b30f5f840f0a79f4af3f71bb452272276d6e7a8e586ff4e9974c6a176b",
+    maxAge: process.env.JWT_MAX_AGE || "7d",
   },
   cookies: {
-    secret: process.env.COOKIES_SECRET,
+    secret:
+      process.env.COOKIES_SECRET ||
+      "2d1ce9821e077c90c5f82fdab273717f499f344a2ab7343d146fafe0c83ece0f",
     // max age in seconds
-    maxAge: process.env.COOKIES_MAX_AGE,
+    maxAge: process.env.COOKIES_MAX_AGE || 604800,
   },
   email: {
     user: process.env.EMAIL_USER,
